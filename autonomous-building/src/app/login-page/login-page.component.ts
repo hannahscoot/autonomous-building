ee7f49e4ec3eb38df4ddd5789c08400aba6561ce
HannahNoMuda <hannah.scott@nomuda.com>
2021-10-08 11:51:50 +0100

    # added login page, no back-end login functionality

--- autonomous-building/src/app/login-page/login-page.component.ts
+++ autonomous-building/src/app/login-page/login-page.component.ts
@@ -9,6 +9,7 @@ export class LoginPageComponent {
     strength = 0;
     validations : boolean[] = [];
     password = '';
+    isLogin = false;
 
     validatePassword() {
         this.validations = [
@@ -20,38 +21,7 @@ export class LoginPageComponent {
         this.strength = this.validations.reduce((acc, cur) => acc + Number(cur), 0)
     }
 
-    function() {
+    newuser() {
+        this.isLogin = !this.isLogin;
     }
-
-
-    /*
-    <label for="password" class="label">Password</label> <!--Password Input Area-->
-<input type='password' name="password" class="input border" placeholder="" [(ngModel)]="password" (ngModelChange)="validatePassword()"/>
-
-<div> <!--This will be the indicator bar underneath password--> 
-    <span class="">0</span>
-    <span class="" *ngIf="(strength > 0)">1</span>
-    <span class="" *ngIf="(strength > 1)">2</span>
-    <span class="" *ngIf="(strength > 2)">3</span>
-    <span class="" *ngIf="(strength > 3)">4</span>
-</div>
-<div *ngIf="(password.length > 0)"> <!--This will be the user response for validation-->
-    <ul>
-        <li>
-            {{validations[0] ? 'Y' : 'N'}} must be at least 5 characters
-        </li>
-        <li>
-            {{validations[1] ? 'Y' : 'N'}} must contain a capital letter
-        </li>
-        <li>
-            {{validations[2] ? 'Y' : 'N'}} must contain a number
-        </li>
-        <li>
-            {{validations[3] ? 'Y' : 'N'}} must contain at least one of $&+,:;=?@#*
-        </li>
-    </ul>
-</div>
-<!--Submit button not able to click until password has necessary strength-->
-<button (click)="function()" [disabled]="(strength<4)">Sign Up</button>
-    */
 }
